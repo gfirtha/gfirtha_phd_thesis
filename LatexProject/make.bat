ac7b7e1f5453dbb5bba3c200df08853b6baf3a21
firtha <firtha@hit.bme.hu>
2017-06-23 13:33:30 +0200

    .new_version_chapter_1

--- LatexProject/make.bat
+++ LatexProject/make.bat
@@ -1,6 +1,6 @@
-pdflatex main
-bibtex main
+pdflatex main --interaction=batchmode
+bibtex main --interaction=batchmode
 makeindex main.nlo -s nomencl.ist -o main.nls
-pdflatex main.tex
-pdflatex main.tex
+pdflatex main.tex --interaction=batchmode
+pdflatex main.tex --interaction=batchmode
 main.pdf
