e7110cce69831f18945402cddf005a5befd6d95c
firtha <firtha@hit.bme.hu>
2017-09-20 10:59:57 +0200

    ...

--- LatexProject/make.bat
+++ LatexProject/make.bat
@@ -1,6 +1,6 @@
-pdflatex main
+pdflatex main -interaction=batchmode
 bibtex main
-makeindex main.nlo -s nomencl.ist -o main.nls
-pdflatex main.tex
-pdflatex main.tex
- main.pdf
+makeindex main.nlo -s nomencl.ist -o main.nl
+pdflatex main.tex -interaction=batchmode
+pdflatex main.tex -interaction=batchmode
+sumatrapdf main.pdf
