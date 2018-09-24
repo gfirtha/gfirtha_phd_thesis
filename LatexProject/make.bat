6600d1cfbc957a560d6e6276197ed95772e978cd
fiala <fiala@hit.bme.hu>
2018-09-24 15:22:35 +0200

    mivan

--- LatexProject/make.bat
+++ LatexProject/make.bat
@@ -1,7 +1,7 @@
-pdflatex main.tex REM -interaction=batchmode
+pdflatex main.tex
 bibtex main
 REM makeindex main.nlo -s nomencl.ist -o main.nl
 makeindex -s nomencl.ist -t main.nlg -o main.nls main.nlo
-pdflatex main_.tex -interaction=batchmode
-pdflatex main.tex -interaction=batchmode
+pdflatex main_.tex
+pdflatex main.tex
 sumatrapdf main.pdf
