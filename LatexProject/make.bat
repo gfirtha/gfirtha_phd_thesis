7ca2838c5cf5ee6c387f12da9be66ae847cf52c2
firtha <firtha@hit.bme.hu>
2018-09-25 16:10:04 +0200

    ..

--- LatexProject/make.bat
+++ LatexProject/make.bat
@@ -3,5 +3,5 @@ bibtex main
 REM makeindex main.nlo -s nomencl.ist -o main.nl
 makeindex -s nomencl.ist -t main.nlg -o main.nls main.nlo
 pdflatex main_.tex -interaction=batchmode
-pdflatex main.tex -interaction=batchmode
+pdflatex main.tex 2>error.txt >output.txt
 sumatrapdf main.pdf
